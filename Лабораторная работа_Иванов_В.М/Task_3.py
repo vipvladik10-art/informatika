a6f18fa1117a22adbb3c04d3fdb854bbdb76cff3
vipvladik10-art <vip.vladik.10@gmail.com>
2026-03-04 15:42:09 +0300

    Delete Лабораторная работа_Иванов_В.М directory

--- Лабораторная работа_Иванов_В.М/Task_3.py
+++
@@ -1,9 +0,0 @@
-list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-
-center_list = len(list_players) // 2 #Делим список поровну
-
-first_team = list_players[:center_list] #С помощью срезов создаём список для первой команды
-second_team = list_players[center_list:] #С помощью срезов создаём список для второй команды
-
-print(first_team) #Выводим список первой команды
-print(second_team) #Выводим список второй команды
